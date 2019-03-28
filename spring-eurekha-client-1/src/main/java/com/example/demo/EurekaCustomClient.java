c1e02c22b5c50b12d80152f430cedb323f7c7590
siva kumar jatla <sivajatla136@gmail.com>
2019-03-28 14:25:44 +0530

    invoking spring-secure app with basic auth

--- spring-eurekha-client-1/src/main/java/com/example/demo/EurekaCustomClient.java
+++ spring-eurekha-client-1/src/main/java/com/example/demo/EurekaCustomClient.java
@@ -2,6 +2,12 @@ package com.example.demo;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
@@ -14,6 +20,7 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,13 +29,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.google.gson.Gson;
+
 @RestController
 public class EurekaCustomClient {
 
 	@Autowired
     RestTemplate restTemplate;
-	//@RequestMapping(value = "/getCollegeStaff", method = RequestMethod.GET)
-	@GetMapping
+	
+	//@GetMapping
+	@RequestMapping(value = "/getCollegeStaff", method = RequestMethod.GET)
 	public ResponseEntity<String> getCollegeStaff() {
 		System.out.println("inside getCollegeStaff");
 		URI url;
@@ -53,10 +63,7 @@ public class EurekaCustomClient {
 			org.springframework.http.HttpHeaders reqhead = new org.springframework.http.HttpHeaders();
 			reqhead.setContentType(org.springframework.http.MediaType.APPLICATION_JSON);
 			
-			JSONObject reqJson = new JSONObject();
-			reqJson.put("id", new String("eurekhacline"));
-			reqJson.put("name", new String("calling spring-rs core"));
-			HttpEntity<Staff> reqEntity = new HttpEntity<>(staff,reqhead);
+			HttpEntity<Staff> reqEntity = new HttpEntity<Staff>(staff,reqhead);
 			
 			
 			responseEntity = restTemplate.exchange(
@@ -74,12 +81,85 @@ public class EurekaCustomClient {
 	            System.out.println(responseEntity.getStatusCode());
 	        }
 		}
-			catch (JSONException e) {
-			// TODO Auto-generated catch block
+			catch (Exception e) {
 			e.printStackTrace();
 		}
 		return responseEntity;
 	}
+	
+	@RequestMapping(value = "/invokeCoreSpringRSReqBodyJson", method = RequestMethod.GET)
+	public ResponseEntity<String> invokeCoreSpringRSReqBodyJson() {
+		System.out.println("inside invokeCoreSpringRS");
+		ResponseEntity<String> responseEntity = null;
+		try {
+			String endUrl = "http://SPRING-RS/spring-rs/vrsecstaffmgmt/saveProfileReqJson";
+			
+			org.springframework.http.HttpHeaders reqhead = new org.springframework.http.HttpHeaders();
+			reqhead.setContentType(org.springframework.http.MediaType.APPLICATION_JSON);
+			
+			
+			
+			
+			JSONObject reqJson = new JSONObject();
+			reqJson.put("id", new String("eurekhacline"));
+			reqJson.put("name", new String("calling spring-rs core"));
+			HttpEntity<String> reqEntity = new HttpEntity<>(reqJson.toString(),reqhead);
+			
+			
+			responseEntity = restTemplate.exchange(
+					endUrl,
+	                HttpMethod.POST,
+	                reqEntity,
+	                String.class
+	        );
+			
+			if (responseEntity.getStatusCode() == HttpStatus.OK) {
+	            System.out.println("response received");
+	            System.out.println(responseEntity.getBody());
+	            //HashMap<String,String> resMap = (HashMap) responseEntity.getBody();
+	            Gson gson = new Gson();
+	            Staff resStaff = gson.fromJson(responseEntity.getBody(), Staff.class);
+	            //System.out.println("resJson::"+resMap.get("id"));
+	            //System.out.println("resentity::"+responseEntity.toString());
+	            System.out.println("resStaff.id::"+resStaff.getId());
+	            System.out.println("resStaff.name::"+resStaff.getName());
+	            
+	            
+	        } else {
+	            System.out.println("error occurred");
+	            System.out.println(responseEntity.getStatusCode());
+	        }
+		}
+			catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return responseEntity;
+	}
+	
+	@RequestMapping(value = "/invokeSpringSecurityApp",method = RequestMethod.GET)
+	public String invokeSpringSecurityApp() {
+		System.out.println("inside invokeSpringSecurityApp");
+		ResponseEntity<String> responseEntity = null;
+		try {
+			String endUrl = "http://spring-security:2222/getWebSiteInfo";
+			org.springframework.http.HttpHeaders reqhead = new org.springframework.http.HttpHeaders();
+			//reqhead.setContentType(org.springframework.http.MediaType.APPLICATION_JSON);
+			reqhead.setBasicAuth("spider","web");
+			HttpEntity<String> reqEntity = new HttpEntity<>(reqhead);
+			responseEntity = restTemplate.exchange(
+					endUrl,
+	                HttpMethod.GET,
+	                reqEntity,
+	                String.class
+	        );
+			System.out.println(responseEntity.getBody());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		return responseEntity.getBody();
+		}
+		
+	
 	
 	@Bean
     @LoadBalanced
