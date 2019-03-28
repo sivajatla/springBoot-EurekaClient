93a131f8046e386f6f6bb43bc46d3713ce205d69
siva kumar jatla <sivajatla136@gmail.com>
2019-03-28 14:28:33 +0530

    configure to register with eureka server

--- spring-boot-security/src/main/java/com/spring/security/LoginController.java
+++ spring-boot-security/src/main/java/com/spring/security/LoginController.java
@@ -2,6 +2,7 @@ package com.spring.security;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
@@ -10,6 +11,13 @@ public class LoginController {
 	public String getWelcomePage() {
 		System.out.println("get welcome");
 		return "welcome.jsp";
+	}
+	
+	@RequestMapping("/getWebSiteInfo")
+	@ResponseBody 
+	public String getWebSiteInfo() {
+		System.out.println("inside getWebSiteInfo");
+		return "this is secure login";
 	}
 
 }
