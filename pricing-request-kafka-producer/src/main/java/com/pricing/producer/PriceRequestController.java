5597dfe6a1ea988afddfcb37e20810d54668ddea
siva kumar jatla <sivajatla136@gmail.com>
2019-04-21 21:36:25 +0530

    converting object to json from json data binding

--- pricing-request-kafka-producer/src/main/java/com/pricing/producer/PriceRequestController.java
+++ pricing-request-kafka-producer/src/main/java/com/pricing/producer/PriceRequestController.java
@@ -1,5 +1,7 @@
 package com.pricing.producer;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,25 +12,40 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @RestController
 @RequestMapping("/price")
 public class PriceRequestController {
 	@Autowired
 	KafkaTemplate<String, String> kafkaTemplate;
-	private static String TOPIC = "test";
-	private static String TOPIC2 = "test2";
+	
+	private static String TOPIC_STRING_DATA = "TOPIC_STRING_DATA";
 	private static String TOPIC_PRICEREQ_JSON = "TOPIC_PRICEREQ_JSON";
+	
 	@GetMapping("/request/{reqPrice}")
 	public String requestPrice(@PathVariable("reqPrice") String amount) {
-		kafkaTemplate.send(TOPIC,amount);
-		return "Requested price pushed to MQ";
+		kafkaTemplate.send(TOPIC_STRING_DATA,amount);
+		return "Requested price pushed to MQ "+TOPIC_STRING_DATA;
 	}
 	
 	@RequestMapping(value = "/priceReqDetails",method=RequestMethod.POST)
 	public String requestPrice(@RequestBody PriceRequest priceReq) {
 		System.out.println("reqpricejson::"+priceReq.toString());
-		kafkaTemplate.send(TOPIC_PRICEREQ_JSON,priceReq.toString());
+		// Creating Object of ObjectMapper define in Jakson Api 
+        ObjectMapper Obj = new ObjectMapper(); 
+        try { 
+            // get Oraganisation object as a json string 
+            String jsonStr = Obj.writeValueAsString(priceReq); 
+            // Displaying JSON String 
+            System.out.println(jsonStr);
+            kafkaTemplate.send(TOPIC_PRICEREQ_JSON,jsonStr);
+        } 
+  
+        catch (IOException e) { 
+            e.printStackTrace(); 
+        } 
+			
 		return "Requested price pushed to the topic::"+TOPIC_PRICEREQ_JSON;
 	}
-
 }
